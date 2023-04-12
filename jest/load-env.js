5817cb9fc580b68c0127a98b3b4a347f963ff950
Jamal Soueidan <jamal@soueidan.com>
2023-04-12 17:17:45 +0200

    update load-env

--- jest/load-env.js
+++ jest/load-env.js
@@ -1,11 +1,11 @@
 const fs = require("fs");
 
-// Read the contents of the local.settings.json file
-const localSettings = JSON.parse(
-  fs.readFileSync("./local.settings.json", "utf-8")
-);
+if (fs.existsSync("./local.settings.json")) {
+  const localSettings = JSON.parse(
+    fs.readFileSync("./local.settings.json", "utf-8")
+  );
 
-// Set environment variables from the "Values" object in local.settings.json
-for (const key in localSettings.Values) {
-  process.env[key] = localSettings.Values[key];
+  for (const key in localSettings.Values) {
+    process.env[key] = localSettings.Values[key];
+  }
 }
