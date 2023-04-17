a8c1304ce94ad0befdd7d7b7e51d4f1b613ee148
Jamal Soueidan <jamal@soueidan.com>
2023-04-17 19:40:37 +0200

    update schema for user

--- src/functions/user/user.types.ts
+++ src/functions/user/user.types.ts
@@ -2,14 +2,16 @@ import { z } from "zod";
 
 export const UserZodSchema = z.object({
   _id: z.string(),
-  fullname: z.string(),
+  fullname: z.string().nonempty().min(8),
   email: z.string().email({ message: "Invalid email address" }),
-  phone: z.string().transform((str) => str.replace(/\+/g, "")),
+  phone: z.string().refine((str) => /^\d{8}$/.test(str), {
+    message: "Invalid phone number format",
+  }),
   active: z.boolean().default(true),
   avatar: z.string().url({ message: "Invalid url" }),
   position: z.string(),
-  postal: z.number(),
-  address: z.string(),
+  postal: z.number().min(4).positive(),
+  address: z.string().nonempty(),
   group: z.string().default("all"),
   language: z.string().default("da"),
   timeZone: z.string().default("Europe/Copenhagen"),
