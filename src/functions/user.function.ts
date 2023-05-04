2f4628675059fa6a9e73d54de40d975f1316884e
Jamal Soueidan <jamal@soueidan.com>
2023-05-04 18:54:47 +0200

    update url for user

--- src/functions/user.function.ts
+++ src/functions/user.function.ts
@@ -9,13 +9,13 @@ import {
 app.http("userCreateOrUpdate", {
   methods: ["PUT"],
   authLevel: "anonymous",
-  route: "customerId/{customerId?",
+  route: "customer/{customerId?",
   handler: UserControllerCreateOrUpdate,
 });
 
 app.http("userGet", {
   methods: ["GET"],
   authLevel: "anonymous",
-  route: "customerId/{customerId?}",
+  route: "customer/{customerId?}",
   handler: UserControllerGet,
 });
