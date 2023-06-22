2f04eba30cce36336cd7bd887ff71c83fa094b12
Jamal Soueidan <jamal@soueidan.com>
2023-06-22 23:51:53 +0200

    feat(validate-address.ts): add name field to LocationServiceValidateAddressSchema

--- src/functions/location/controllers/validate-address.ts
+++ src/functions/location/controllers/validate-address.ts
@@ -8,6 +8,7 @@ export type LocationControllerValidateAddressRequest = {
 };
 
 export const LocationServiceValidateAddressSchema = LocationZodSchema.pick({
+  name: true,
   fullAddress: true,
 });
 
