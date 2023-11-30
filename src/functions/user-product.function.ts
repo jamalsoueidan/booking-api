adb0a9e158981f381b4d3f968f62a36159b7b1eb
Jamal Soueidan <jamal@soueidan.com>
2023-11-30 08:59:35 +0300

    feat(user-products): add GET endpoint to retrieve a specific user product
    This commit adds a new GET endpoint to retrieve a specific user product. The endpoint is located at `/user/{username}/products/{productId}`. It accepts two path parameters: `username` and `productId`. The endpoint is authenticated anonymously.
    
    The OpenAPI specification has been updated to include the new endpoint. The response schema for the endpoint has also been added.
    
    A new controller function `UserProductsControllerGet` has been implemented to handle the GET request. It validates the query parameters and retrieves the user's customer ID using the `UserServiceGetCustomerId` function. Then, it calls the `CustomerProductServiceGet` function to retrieve the specific user product based on the customer ID and product ID.
    
    Unit tests have been added to ensure the correct functionality of the `UserProductsControllerGet` function. The tests cover the scenario of successfully retrieving a product inside a schedule.
    
    Note: The implementation of the `UserServiceGet

--- src/functions/user-product.function.ts
+++ src/functions/user-product.function.ts
@@ -2,6 +2,7 @@ import "module-alias/register";
 import { UserProductsControllerListProductsByLocation } from "./user/controllers/products/list-by-location";
 
 import { app } from "@azure/functions";
+import { UserProductsControllerGet } from "./user/controllers/products/get";
 import { UserProductsControllerGetProductsByLocation } from "./user/controllers/products/get-products";
 import { UserProductsControllerListBySchedule } from "./user/controllers/products/list-by-schedule";
 
@@ -24,4 +25,11 @@ app.http("UserProductsControllerGetProducts", {
   authLevel: "anonymous",
   route: "user/{username}/products/location/{locationId}",
   handler: UserProductsControllerGetProductsByLocation,
+});
+
+app.http("UserProductsControllerGet", {
+  methods: ["GET"],
+  authLevel: "anonymous",
+  route: "user/{username}/products/{productId}",
+  handler: UserProductsControllerGet,
 });
---
+++ src/functions/user/controllers/products/get.spec.ts
@@ -0,0 +1,77 @@
+import { HttpRequest, InvocationContext } from "@azure/functions";
+import { CustomerProductServiceUpsert } from "~/functions/customer/services/product";
+import { CustomerScheduleServiceCreate } from "~/functions/customer/services/schedule/create";
+
+import { TimeUnit } from "~/functions/schedule";
+
+import {
+  HttpSuccessResponse,
+  createContext,
+  createHttpRequest,
+} from "~/library/jest/azure";
+import { createUser } from "~/library/jest/helpers";
+import { getProductObject } from "~/library/jest/helpers/product";
+import {
+  UserProductsControllerGet,
+  UserProductsControllerGetRequest,
+  UserProductsControllerGetResponse,
+} from "./get";
+
+require("~/library/jest/mongoose/mongodb.jest");
+
+describe("UserProductsControllerGet", () => {
+  let context: InvocationContext;
+  let request: HttpRequest;
+  const productId = 1000;
+  const product = getProductObject({
+    variantId: 1,
+    duration: 60,
+    breakTime: 0,
+    noticePeriod: {
+      value: 1,
+      unit: TimeUnit.DAYS,
+    },
+    bookingPeriod: {
+      value: 1,
+      unit: TimeUnit.WEEKS,
+    },
+    locations: [],
+  });
+
+  beforeEach(async () => {
+    context = createContext();
+  });
+
+  it("should be able to get product inside schedule", async () => {
+    const user = await createUser({ customerId: 134 });
+    const newSchedule = await CustomerScheduleServiceCreate({
+      name: "asd",
+      customerId: user.customerId,
+    });
+
+    const newProduct = await CustomerProductServiceUpsert(
+      {
+        customerId: newSchedule.customerId,
+        productId,
+      },
+      { ...product, scheduleId: newSchedule._id }
+    );
+
+    expect(newProduct?.scheduleId.toString()).toEqual(
+      newSchedule._id.toString()
+    );
+
+    request = await createHttpRequest<UserProductsControllerGetRequest>({
+      query: {
+        username: user.username || "",
+        productId,
+      },
+    });
+
+    const res: HttpSuccessResponse<UserProductsControllerGetResponse> =
+      await UserProductsControllerGet(request, context);
+
+    expect(res.jsonBody?.success).toBeTruthy();
+    expect(res.jsonBody?.payload?.productId).toBe(productId);
+  });
+});
