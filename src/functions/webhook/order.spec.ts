05c2e705a8374e5229a26fddcf44fc9536f92a34
Jamal Soueidan <jamal@soueidan.com>
2023-12-08 21:23:58 +0300

    feat(webhook-order.function.ts): Refactor webhook-order function to use Azure Functions v4 syntax and add support for processing webhook orders feat(webhook/order.spec.ts): Add unit tests for webhookOrderProcess function fix(webhook/order.ts): Update webhookOrderProcess function to update existing orders or create new orders if they don't exist

---
+++ src/functions/webhook/order.spec.ts
@@ -0,0 +1,31 @@
+import { InvocationContext } from "@azure/functions";
+import { createContext } from "~/library/jest/azure";
+import { orderNofulfillment } from "./data-ordre";
+import { orderWithfulfillmentAndRefunds } from "./data-with-fullfilment-and-refunds";
+import { webhookOrderProcess } from "./order";
+
+require("~/library/jest/mongoose/mongodb.jest");
+
+jest.mock("~/library/application-insight", () => ({
+  telemetryClient: {
+    trackException: jest.fn(),
+  },
+}));
+
+describe("webhookOrderProcess", () => {
+  let context: InvocationContext = createContext();
+
+  it("Should be able to add body to order and update it", async () => {
+    let res = await webhookOrderProcess(orderNofulfillment, context);
+
+    expect(res).toBeDefined();
+    expect(res?.fulfillments.length).toBe(0);
+    expect(res?.refunds.length).toBe(0);
+
+    res = await webhookOrderProcess(orderWithfulfillmentAndRefunds, context);
+
+    expect(res).toBeDefined();
+    expect(res?.fulfillments.length).toBe(5);
+    expect(res?.refunds.length).toBe(1);
+  });
+});
