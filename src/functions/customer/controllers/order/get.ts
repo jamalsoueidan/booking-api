136cad027313508a63187b58ca1371caac7f64fb
Jamal Soueidan <jamal@soueidan.com>
2023-12-09 22:50:26 +0300

    feat(customer-order): add customer order function and controllers
    - Add a new file `customer-order.function.ts` to handle customer order related requests
    - Implement `CustomerOrderControllerGet` function in `customer/controllers/order/get.ts` to handle GET requests for retrieving customer order details
    - Implement `CustomerOrderControllerList` function in `customer/controllers/order/list.ts` to handle GET requests for listing customer orders
    - Define request and response types for `CustomerOrderControllerGet` and `CustomerOrderControllerList`
    - Update `order.schema.ts` to remove unused schemas and properties
    - Update `order.types.ts` to remove unused types and properties

---
+++ src/functions/customer/controllers/order/get.ts
@@ -0,0 +1,24 @@
+import { z } from "zod";
+import { _ } from "~/library/handler";
+import { NumberOrStringType } from "~/library/zod";
+import { CustomerOrderServiceGet } from "../../services/order/get";
+
+export type CustomerOrderControllerGetRequest = {
+  query: z.infer<typeof CustomerOrderControllerGetSchema>;
+};
+
+export const CustomerOrderControllerGetSchema = z.object({
+  customerId: NumberOrStringType,
+  lineItemId: NumberOrStringType,
+});
+
+export type CustomerOrderControllerGetResponse = Awaited<
+  ReturnType<typeof CustomerOrderServiceGet>
+>;
+
+export const CustomerOrderControllerGet = _(
+  async ({ query }: CustomerOrderControllerGetRequest) => {
+    const validateData = CustomerOrderControllerGetSchema.parse(query);
+    return CustomerOrderServiceGet(validateData);
+  }
+);
---
+++ src/functions/customer/controllers/order/list.ts
@@ -0,0 +1,26 @@
+import { _ } from "~/library/handler";
+
+import { z } from "zod";
+import { NumberOrStringType } from "~/library/zod";
+import { CustomerOrderServiceList } from "../../services/order/list";
+
+export type CustomerOrderControllerListRequest = {
+  query: z.infer<typeof CustomerOrderControllerListSchema>;
+};
+
+export const CustomerOrderControllerListSchema = z.object({
+  customerId: NumberOrStringType,
+  year: NumberOrStringType,
+  month: NumberOrStringType,
+});
+
+export type CustomerOrderControllerListResponse = Awaited<
+  ReturnType<typeof CustomerOrderServiceList>
+>;
+
+export const CustomerOrderControllerList = _(
+  async ({ query }: CustomerOrderControllerListRequest) => {
+    const validateData = CustomerOrderControllerListSchema.parse(query);
+    return CustomerOrderServiceList(validateData);
+  }
+);
