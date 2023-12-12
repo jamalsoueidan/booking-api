e808ec0a8a3aa36d74d38fb57016213e04600a97
Jamal Soueidan <jamal@soueidan.com>
2023-12-12 04:38:56 +0300

    refactor(rates.ts): refactor ShippingControllerRates function to use Azure Functions types and log request details feat(rates.ts): return response as HttpResponseInit with jsonBody
    fix(rates.spec.ts): fix indentation and formatting in shippingBody object
    feat(rates.spec.ts): add additional item to shippingBody object for testing purposes
    
    fix(rates.ts): fix import statement for mongoose connect function
    fix(rates.ts): change body parameter name to rate in ShippingServiceRates function
    fix(rates.ts): fix variable name in ShippingServiceRates function
    fix(rates.ts): fix variable name in ShippingServiceRates function
    fix(rates.ts): fix variable name in ShippingServiceRates function
    fix(rates.ts): fix variable name in ShippingServiceRates function
    fix(rates.ts): fix variable name in ShippingServiceRates function
    fix(rates.ts): fix variable name in ShippingServiceRates function
    fix(rates.ts): fix variable name in ShippingServiceRates function
    fix(rates.ts): fix variable name in ShippingServiceRates function
    fix(rates.ts): fix variable name in ShippingServiceRates function
    fix(rates.ts): fix variable name in ShippingServiceRates function
    fix(rates.ts): fix variable name in ShippingServiceRates function
    fix(rates.ts): fix variable name in ShippingServiceRates

--- src/functions/shipping/services/rates.ts
+++ src/functions/shipping/services/rates.ts
@@ -1,72 +1,77 @@
 import mongoose from "mongoose";
 
+import { connect } from "~/library/mongoose";
 import { ShippingModel } from "../shipping.model";
 
 export type ShippingBody = {
-  origin: {
-    country: string;
-    postal_code: string;
-    province: any;
-    city: string;
-    name: any;
-    address1: string;
-    address2: string;
-    address3: any;
-    latitude: number;
-    longitude: number;
-    phone: string;
-    fax: any;
-    email: any;
-    address_type: any;
-    company_name: string;
-  };
-  destination: {
-    country: string;
-    postal_code: string;
-    province: any;
-    city: string;
-    name: string;
-    address1: string;
-    address2: any;
-    address3: any;
-    latitude: number;
-    longitude: number;
-    phone: any;
-    fax: any;
-    email: any;
-    address_type: any;
-    company_name: any;
-  };
-  items: Array<{
-    name: string;
-    sku: string;
-    quantity: number;
-    grams: number;
-    price: number;
-    vendor: string;
-    requires_shipping: boolean;
-    taxable: any;
-    fulfillment_service: string;
-    properties?: {
-      _from: string;
-      _to: string;
-      _customerId: string;
-      Dato: string;
-      Tid: string;
-      Behandler: string;
-      Varighed: string;
-      _shippingId?: string;
-      _freeShipping?: string;
+  rate: {
+    origin: {
+      country: string;
+      postal_code: string;
+      province: any;
+      city: string;
+      name: any;
+      address1: string;
+      address2: string;
+      address3: any;
+      latitude: number;
+      longitude: number;
+      phone: string;
+      fax: any;
+      email: any;
+      address_type: any;
+      company_name: string;
     };
-    product_id: number;
-    variant_id: number;
-  }>;
-  currency: string;
-  locale: string;
+    destination: {
+      country: string;
+      postal_code: string;
+      province: any;
+      city: string;
+      name: string;
+      address1: string;
+      address2: any;
+      address3: any;
+      latitude: number;
+      longitude: number;
+      phone: any;
+      fax: any;
+      email: any;
+      address_type: any;
+      company_name: any;
+    };
+    items: Array<{
+      name: string;
+      sku: string;
+      quantity: number;
+      grams: number;
+      price: number;
+      vendor: string;
+      requires_shipping: boolean;
+      taxable: any;
+      fulfillment_service: string;
+      properties?: {
+        _from: string;
+        _to: string;
+        _customerId: string;
+        Dato: string;
+        Tid: string;
+        Behandler: string;
+        Varighed: string;
+        _shippingId?: string;
+        _freeShipping?: string;
+      };
+      product_id: number;
+      variant_id: number;
+    }>;
+    currency: string;
+    locale: string;
+  };
 };
 
 export const ShippingServiceRates = async (body: ShippingBody) => {
-  let shippingIds = body.items
+  await connect();
+
+  let shippingIds = body.rate.items
     .map((item) => item.properties?._shippingId)
     .filter((id) => typeof id === "string"); // Ensure only numbers
 
@@ -75,7 +80,7 @@ export const ShippingServiceRates = async (body: ShippingBody) => {
   const onMinDate = new Date(Date.now() + 86400000).toISOString(); // +1 day
   const onMaxDate = new Date(Date.now() + 2 * 86400000).toISOString(); // +2 days
 
-  const hasFreeShipping = body.items.some(
+  const hasFreeShipping = body.rate.items.some(
     (item) => item.properties?._freeShipping === "true"
   );
 
@@ -83,7 +88,7 @@ export const ShippingServiceRates = async (body: ShippingBody) => {
     return {
       rates: [
         {
-          service_name: `Gratis leverings`,
+          service_name: `Gratis levering`,
           service_code: "ETON",
           total_price: 0,
           currency: "DKK",
