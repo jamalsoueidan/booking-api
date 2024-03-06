90145f79ac25f1363df5b46f47b941ea9370c38c
Jamal Soueidan <jamal@soueidan.com>
2024-03-06 13:44:07 +0100

    Refactor blocked range controller to use CustomerBlockedServiceRange with start/end params

--- src/functions/customer/controllers/blocked/range.ts
+++ src/functions/customer/controllers/blocked/range.ts
@@ -1,7 +1,7 @@
 import { z } from "zod";
 import { _ } from "~/library/handler";
-import { NumberOrStringType, StringOrObjectIdType } from "~/library/zod";
-import { CustomerBlockedServiceList } from "../../services/blocked/list";
+import { NumberOrStringType } from "~/library/zod";
+import { CustomerBlockedServiceRange } from "../../services/blocked/range";
 
 export type CustomerBlockedControllerRangeRequest = {
   query: z.infer<typeof CustomerBlockedControllerRangeQuerySchema>;
@@ -9,17 +9,17 @@ export type CustomerBlockedControllerRangeRequest = {
 
 export const CustomerBlockedControllerRangeQuerySchema = z.object({
   customerId: NumberOrStringType,
-  limit: NumberOrStringType,
-  nextCursor: StringOrObjectIdType,
+  start: z.string(),
+  end: z.string(),
 });
 
 export type CustomerBlockedControllerRangeQueryResponse = Awaited<
-  ReturnType<typeof CustomerBlockedServiceList>
+  ReturnType<typeof CustomerBlockedServiceRange>
 >;
 
 export const CustomerBlockedControllerRange = _(
   ({ query }: CustomerBlockedControllerRangeRequest) => {
     const validateData = CustomerBlockedControllerRangeQuerySchema.parse(query);
-    return CustomerBlockedServiceList(validateData);
+    return CustomerBlockedServiceRange(validateData);
   }
 );
