116c1f1b3c1dd70c8dfa4edbe124d6e1a7cef360
Jamal Soueidan <jamal@soueidan.com>
2024-03-12 13:06:14 +0100

    Add generate-types step to workflows and refine GraphQL type documents path

--- .graphqlrc.ts
+++ .graphqlrc.ts
@@ -1,14 +1,13 @@
 import { ApiType, shopifyApiProject } from "@shopify/api-codegen-preset";
 
 export default {
-  // For syntax highlighting / auto-complete when writing operations
   schema: "https://shopify.dev/admin-graphql-direct-proxy/2023-10",
-  documents: ["*.ts", "!node_modules"],
+  documents: ["./src/**/*.{ts,tsx}"],
   projects: {
-    // To produce variable / return types for Admin API operations
     default: shopifyApiProject({
       apiType: ApiType.Admin,
       apiVersion: "2023-10",
+      documents: ["./src/**/*.{ts,tsx}"],
       outputDir: "./src/types",
     }),
   },
--- src/types/admin.generated.d.ts
+++ src/types/admin.generated.d.ts
@@ -1,105 +1,52 @@
 /* eslint-disable eslint-comments/disable-enable-pair */
 /* eslint-disable eslint-comments/no-unlimited-disable */
 /* eslint-disable */
-import * as AdminTypes from "./admin.types";
+import * as AdminTypes from './admin.types.d.ts';
 
 export type ProductVariantCreateMutationVariables = AdminTypes.Exact<{
   input: AdminTypes.ProductVariantInput;
 }>;
 
-export type ProductVariantCreateMutation = {
-  productVariantCreate?: AdminTypes.Maybe<{
-    product?: AdminTypes.Maybe<Pick<AdminTypes.Product, "id" | "title">>;
-    productVariant?: AdminTypes.Maybe<
-      Pick<
-        AdminTypes.ProductVariant,
-        | "createdAt"
-        | "displayName"
-        | "id"
-        | "inventoryPolicy"
-        | "inventoryQuantity"
-        | "price"
-        | "title"
-      > & { product: Pick<AdminTypes.Product, "id"> }
-    >;
-    userErrors: Array<Pick<AdminTypes.UserError, "field" | "message">>;
-  }>;
-};
+
+export type ProductVariantCreateMutation = { productVariantCreate?: AdminTypes.Maybe<{ product?: AdminTypes.Maybe<Pick<AdminTypes.Product, 'id' | 'title'>>, productVariant?: AdminTypes.Maybe<(
+      Pick<AdminTypes.ProductVariant, 'createdAt' | 'displayName' | 'id' | 'inventoryPolicy' | 'inventoryQuantity' | 'price' | 'title'>
+      & { product: Pick<AdminTypes.Product, 'id'> }
+    )>, userErrors: Array<Pick<AdminTypes.UserError, 'field' | 'message'>> }> };
 
 export type FileCreateMutationVariables = AdminTypes.Exact<{
   files: Array<AdminTypes.FileCreateInput> | AdminTypes.FileCreateInput;
 }>;
 
-export type FileCreateMutation = {
-  fileCreate?: AdminTypes.Maybe<{
-    files?: AdminTypes.Maybe<
-      Array<
-        | Pick<AdminTypes.GenericFile, "fileStatus" | "alt">
-        | Pick<AdminTypes.MediaImage, "fileStatus" | "alt">
-        | Pick<AdminTypes.Video, "fileStatus" | "alt">
-      >
-    >;
-    userErrors: Array<Pick<AdminTypes.FilesUserError, "field" | "message">>;
-  }>;
-};
+
+export type FileCreateMutation = { fileCreate?: AdminTypes.Maybe<{ files?: AdminTypes.Maybe<Array<Pick<AdminTypes.GenericFile, 'fileStatus' | 'alt'> | Pick<AdminTypes.MediaImage, 'fileStatus' | 'alt'> | Pick<AdminTypes.Video, 'fileStatus' | 'alt'>>>, userErrors: Array<Pick<AdminTypes.FilesUserError, 'field' | 'message'>> }> };
 
 export type FileGetQueryVariables = AdminTypes.Exact<{
-  query: AdminTypes.Scalars["String"]["input"];
+  query: AdminTypes.Scalars['String']['input'];
 }>;
 
-export type FileGetQuery = {
-  files: {
-    nodes: Array<{
-      preview?: AdminTypes.Maybe<{
-        image?: AdminTypes.Maybe<
-          Pick<AdminTypes.Image, "url" | "width" | "height">
-        >;
-      }>;
-    }>;
-  };
-};
+
+export type FileGetQuery = { files: { nodes: Array<{ preview?: AdminTypes.Maybe<{ image?: AdminTypes.Maybe<Pick<AdminTypes.Image, 'url' | 'width' | 'height'>> }> }> } };
 
 export type StagedUploadsCreateMutationVariables = AdminTypes.Exact<{
   input: Array<AdminTypes.StagedUploadInput> | AdminTypes.StagedUploadInput;
 }>;
 
-export type StagedUploadsCreateMutation = {
-  stagedUploadsCreate?: AdminTypes.Maybe<{
-    stagedTargets?: AdminTypes.Maybe<
-      Array<
-        Pick<AdminTypes.StagedMediaUploadTarget, "resourceUrl" | "url"> & {
-          parameters: Array<
-            Pick<AdminTypes.StagedUploadParameter, "name" | "value">
-          >;
-        }
-      >
-    >;
-    userErrors: Array<Pick<AdminTypes.UserError, "field" | "message">>;
-  }>;
-};
+
+export type StagedUploadsCreateMutation = { stagedUploadsCreate?: AdminTypes.Maybe<{ stagedTargets?: AdminTypes.Maybe<Array<(
+      Pick<AdminTypes.StagedMediaUploadTarget, 'resourceUrl' | 'url'>
+      & { parameters: Array<Pick<AdminTypes.StagedUploadParameter, 'name' | 'value'>> }
+    )>>, userErrors: Array<Pick<AdminTypes.UserError, 'field' | 'message'>> }> };
 
 interface GeneratedQueryTypes {
-  "#graphql\n  query FileGet($query: String!) {\n    files(first: 10, sortKey: UPDATED_AT, reverse: true, query: $query) {\n      nodes {\n        preview {\n          image {\n            url\n            width\n            height\n          }\n        }\n      }\n    }\n  }\n": {
-    return: FileGetQuery;
-    variables: FileGetQueryVariables;
-  };
+  "#graphql\n  query FileGet($query: String!) {\n    files(first: 10, sortKey: UPDATED_AT, reverse: true, query: $query) {\n      nodes {\n        preview {\n          image {\n            url\n            width\n            height\n          }\n        }\n      }\n    }\n  }\n": {return: FileGetQuery, variables: FileGetQueryVariables},
 }
 
 interface GeneratedMutationTypes {
-  "#graphql\n  mutation productVariantCreate($input: ProductVariantInput!) {\n    productVariantCreate(input: $input) {\n      product {\n        id\n        title\n      }\n      productVariant {\n        createdAt\n        displayName\n        id\n        inventoryPolicy\n        inventoryQuantity\n        price\n        product {\n          id\n        }\n        title\n      }\n      userErrors {\n        field\n        message\n      }\n    }\n  }\n": {
-    return: ProductVariantCreateMutation;
-    variables: ProductVariantCreateMutationVariables;
-  };
-  "#graphql\n  mutation fileCreate($files: [FileCreateInput!]!) {\n    fileCreate(files: $files) {\n      files {\n        fileStatus\n        alt\n      }\n      userErrors {\n        field\n        message\n      }\n    }\n  }\n": {
-    return: FileCreateMutation;
-    variables: FileCreateMutationVariables;
-  };
-  "#graphql\n  mutation stagedUploadsCreate($input: [StagedUploadInput!]!) {\n    stagedUploadsCreate(input: $input) {\n      stagedTargets {\n        resourceUrl\n        url\n        parameters {\n          name\n          value\n        }\n      }\n      userErrors {\n        field\n        message\n      }\n    }\n  }\n": {
-    return: StagedUploadsCreateMutation;
-    variables: StagedUploadsCreateMutationVariables;
-  };
+  "#graphql\n  mutation productVariantCreate($input: ProductVariantInput!) {\n    productVariantCreate(input: $input) {\n      product {\n        id\n        title\n      }\n      productVariant {\n        createdAt\n        displayName\n        id\n        inventoryPolicy\n        inventoryQuantity\n        price\n        product {\n          id\n        }\n        title\n      }\n      userErrors {\n        field\n        message\n      }\n    }\n  }\n": {return: ProductVariantCreateMutation, variables: ProductVariantCreateMutationVariables},
+  "#graphql\n  mutation fileCreate($files: [FileCreateInput!]!) {\n    fileCreate(files: $files) {\n      files {\n        fileStatus\n        alt\n      }\n      userErrors {\n        field\n        message\n      }\n    }\n  }\n": {return: FileCreateMutation, variables: FileCreateMutationVariables},
+  "#graphql\n  mutation stagedUploadsCreate($input: [StagedUploadInput!]!) {\n    stagedUploadsCreate(input: $input) {\n      stagedTargets {\n        resourceUrl\n        url\n        parameters {\n          name\n          value\n        }\n      }\n      userErrors {\n        field\n        message\n      }\n    }\n  }\n": {return: StagedUploadsCreateMutation, variables: StagedUploadsCreateMutationVariables},
 }
-declare module "@shopify/admin-api-client" {
+declare module '@shopify/admin-api-client' {
   type InputMaybe<T> = AdminTypes.InputMaybe<T>;
   interface AdminQueries extends GeneratedQueryTypes {}
   interface AdminMutations extends GeneratedMutationTypes {}
