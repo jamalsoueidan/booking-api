7268739e0a6978e728bfc6b6b4e2768ee099536b
Jamal Soueidan <jamal@soueidan.com>
2024-03-24 05:51:36 +0100

    Filter out deleted locations in customer service status count

--- src/functions/customer/services/customer.ts
+++ src/functions/customer/services/customer.ts
@@ -81,7 +81,10 @@ export const CustomerServiceStatus = async ({
   const user = await UserModel.findOne({ customerId });
   if (user) {
     const schedule = await ScheduleModel.count({ customerId });
-    const locations = await LocationModel.count({ customerId });
+    const locations = await LocationModel.count({
+      customerId,
+      deletedAt: undefined,
+    });
     const services = await ScheduleModel.count({
       customerId,
       "products.0": { $exists: true },
