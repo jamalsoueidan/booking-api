84c0783913c35c70f11dcf39332b92df1b7bb8f2
Jamal Soueidan <jamal@soueidan.com>
2024-03-31 08:33:06 +0200

    Refactor payout log schema to support multiple reference types

--- src/functions/payout-log/payout-log.types.ts
+++ src/functions/payout-log/payout-log.types.ts
@@ -1,11 +1,16 @@
 import { z } from "zod";
 import { NumberOrStringType, ObjectIdType } from "~/library/zod";
 
+export enum PayoutLogReferenceType {
+  SHIPPING = "Shipping",
+  LINE_ITEM = "LineItem",
+}
+
 export const PayoutLogZodSchema = z.object({
   customerId: NumberOrStringType,
-  lineItemId: NumberOrStringType,
+  referenceType: z.nativeEnum(PayoutLogReferenceType),
+  referenceId: NumberOrStringType,
   payout: ObjectIdType,
-  createdAt: z.date(),
 });
 
 export type PayoutLog = z.infer<typeof PayoutLogZodSchema>;
