87f774556a18777670b9ef517addd2d8cd04f4b9
Jamal Soueidan <jamal@soueidan.com>
2024-04-10 09:24:35 +0200

    Add webhookCustomerDelete endpoint and related model imports

--- src/functions/webhook-customer.function.ts
+++ src/functions/webhook-customer.function.ts
@@ -2,6 +2,9 @@ import "module-alias/register";
 
 import { HttpRequest, InvocationContext, app } from "@azure/functions";
 import { connect } from "~/library/mongoose";
+import { BlockedModel } from "./blocked/blocked.model";
+import { LocationModel } from "./location";
+import { ScheduleModel } from "./schedule";
 import { UserModel } from "./user";
 
 export type Customer = {
@@ -21,6 +24,38 @@ app.http("webhookCustomerUpdate", {
     const response = await UserModel.updateOne({ customerId }, { active });
     context.log(
       `Customer Update, customerId = '${customerId}', active = '${active}', update = '${response.modifiedCount}'`
+    );
+    return { body: "" };
+  },
+});
+
+app.http("webhookCustomerDelete", {
+  methods: ["POST"],
+  authLevel: "anonymous",
+  route: "webhooks/customer/delete",
+  handler: async (request: HttpRequest, context: InvocationContext) => {
+    await connect();
+    const customer = (await request.json()) as unknown as Customer;
+    const customerId = customer.id;
+    const response = await UserModel.deleteOne({
+      customerId,
+      email: { $regex: "@example\\.com$", $options: "i" },
+    });
+
+    if (response.deletedCount > 0) {
+      ScheduleModel.deleteMany({
+        customerId,
+      });
+      LocationModel.deleteMany({
+        customerId,
+      });
+      BlockedModel.deleteMany({
+        customerId,
+      });
+    }
+
+    context.log(
+      `Customer Delete, customerId = '${customerId}', update = '${response.deletedCount}'`
     );
     return { body: "" };
   },
