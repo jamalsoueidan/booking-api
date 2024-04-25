66d3310aa2ab8ab709e0ca5b8bd1752777b58d35
Jamal Soueidan <jamal@soueidan.com>
2024-04-25 23:34:05 +0200

    Expand Customer type with additional fields and update UserModel in webhookCustomerUpdate function

--- src/functions/webhook-customer.function.ts
+++ src/functions/webhook-customer.function.ts
@@ -9,8 +9,74 @@ import { UserModel } from "./user";
 
 export type Customer = {
   id: number;
-  tags: string;
   email: string;
+  created_at: string;
+  updated_at: string;
+  first_name: string;
+  last_name: string;
+  orders_count: number;
+  state: string;
+  total_spent: string;
+  last_order_id: number;
+  note: string;
+  verified_email: boolean;
+  multipass_identifier: any;
+  tax_exempt: boolean;
+  tags: string;
+  last_order_name: string;
+  currency: string;
+  phone: string;
+  addresses: Array<{
+    id: number;
+    customer_id: number;
+    first_name: string;
+    last_name: string;
+    company?: string;
+    address1: string;
+    address2?: string;
+    city: string;
+    province?: string;
+    country: string;
+    zip: string;
+    phone: string;
+    name: string;
+    province_code: any;
+    country_code: string;
+    country_name: string;
+    default: boolean;
+  }>;
+  tax_exemptions: Array<any>;
+  email_marketing_consent: {
+    state: string;
+    opt_in_level: string;
+    consent_updated_at: string;
+  };
+  sms_marketing_consent: {
+    state: string;
+    opt_in_level: string;
+    consent_updated_at: any;
+    consent_collected_from: string;
+  };
+  admin_graphql_api_id: string;
+  default_address: {
+    id: number;
+    customer_id: number;
+    first_name: string;
+    last_name: string;
+    company: any;
+    address1: string;
+    address2: any;
+    city: string;
+    province: any;
+    country: string;
+    zip: string;
+    phone: string;
+    name: string;
+    province_code: any;
+    country_code: string;
+    country_name: string;
+    default: boolean;
+  };
 };
 
 app.http("webhookCustomerUpdate", {
@@ -24,7 +90,12 @@ app.http("webhookCustomerUpdate", {
     const customerId = customer.id;
     const response = await UserModel.updateOne(
       { customerId },
-      { active, email: customer.email }
+      {
+        active,
+        email: customer.email,
+        fullname: `${customer.first_name} ${customer.last_name}`,
+        phone: customer.phone,
+      }
     );
     context.log(
       `Customer Update, customerId = '${customerId}', active = '${active}', update = '${response.modifiedCount}'`
