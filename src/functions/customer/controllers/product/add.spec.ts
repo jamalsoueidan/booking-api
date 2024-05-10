a012747192b805013dc409814c2f46d6633bf9aa
Jamal Soueidan <jamal@soueidan.com>
2024-05-10 18:05:17 +0200

    Add mongoose schema and update product handling in add.spec.ts
     • Introduce mongoose for ObjectId handling in product locations.
     • Add new types LocationOriginTypes, LocationTypes, and ScheduleProduct.
     • Implement ProductUpdateMutation and ProductPriceUpdateMutation for product updates.
     • Create new schedule and update product tags with user, treatment, and schedule details.

--- src/functions/customer/controllers/product/add.spec.ts
+++ src/functions/customer/controllers/product/add.spec.ts
@@ -6,11 +6,18 @@ import {
   createHttpRequest,
 } from "~/library/jest/azure";
 
+import mongoose from "mongoose";
+import { LocationOriginTypes, LocationTypes } from "~/functions/location";
+import { ScheduleProduct } from "~/functions/schedule";
 import { createUser } from "~/library/jest/helpers";
 import { getProductObject } from "~/library/jest/helpers/product";
 import { shopifyAdmin } from "~/library/shopify";
 import { GidFormat } from "~/library/zod";
-import { ProductDuplicateMutation } from "~/types/admin.generated";
+import {
+  ProductDuplicateMutation,
+  ProductPricepdateMutation,
+  ProductUpdateMutation,
+} from "~/types/admin.generated";
 import { CustomerScheduleServiceCreate } from "../../services/schedule/create";
 import {
   CustomerProductControllerAdd,
@@ -28,102 +35,200 @@ jest.mock("@shopify/admin-api-client", () => ({
 
 const mockRequest = shopifyAdmin.request as jest.Mock;
 
-const mockProduct: ProductDuplicateMutation = {
-  productDuplicate: {
-    newProduct: {
-      id: "gid://shopify/Product/9196220121415",
-      handle: "testerne-new-product",
-      variants: {
-        nodes: [
-          {
-            id: "gid://shopify/ProductVariant/49511289782599",
-            compareAtPrice: "150.00",
-            price: "90.00",
-          },
-        ],
-      },
-      parentId: {
-        id: "gid://shopify/Metafield/44429081510215",
-        value: "gid://shopify/Product/8022089105682",
-      },
-      scheduleId: {
-        id: "gid://shopify/Metafield/44429081542983",
-        value: "schedule",
-      },
-      locations: {
-        id: "gid://shopify/Metafield/44429081411911",
-        value: '{"locations":[]}',
-      },
-      bookingPeriodValue: {
-        id: "gid://shopify/Metafield/44429081313607",
-        value: "1",
-      },
-      bookingPeriodUnit: {
-        id: "gid://shopify/Metafield/44429081280839",
-        value: "months",
-      },
-      noticePeriodValue: {
-        id: "gid://shopify/Metafield/44429081477447",
-        value: "1",
-      },
-      noticePeriodUnit: {
-        id: "gid://shopify/Metafield/44429081444679",
-        value: "hours",
-      },
-      duration: {
-        id: "gid://shopify/Metafield/44429081379143",
-        value: "60",
-      },
-      breaktime: {
-        id: "gid://shopify/Metafield/44429081346375",
-        value: "10",
-      },
-    },
-  },
-};
-
 describe("CustomerProductControllerAdd", () => {
   let context: InvocationContext;
   let request: HttpRequest;
   const customerId = 123;
   const productId = 1000;
+  let mockProduct: ProductDuplicateMutation;
+  let productBody: Pick<
+    ScheduleProduct,
+    "parentId" | "locations" | "price" | "compareAtPrice"
+  >;
+  let title: string = "title product";
 
   beforeEach(async () => {
     context = createContext();
     jest.clearAllMocks();
+
+    productBody = {
+      parentId: 8022089105682,
+      locations: [
+        {
+          location: new mongoose.Types.ObjectId(),
+          locationType: LocationTypes.DESTINATION,
+          originType: LocationOriginTypes.COMMERCIAL,
+        },
+      ],
+      price: {
+        amount: "100",
+        currencyCode: "DKK",
+      },
+      compareAtPrice: {
+        amount: "150",
+        currencyCode: "DKK",
+      },
+    };
+
+    mockProduct = {
+      productDuplicate: {
+        newProduct: {
+          id: "gid://shopify/Product/1",
+          handle: "testerne-new-product",
+          variants: {
+            nodes: [
+              {
+                id: "gid://shopify/ProductVariant/49511289782599",
+                compareAtPrice: "0.00",
+                price: "0.00",
+              },
+            ],
+          },
+          parentId: {
+            id: `gid://shopify/Metafield/44429081510215`,
+            value: `gid://shopify/Product/${productBody.parentId}`,
+          },
+          scheduleId: {
+            id: "gid://shopify/Metafield/44429081542983",
+            value: "",
+          },
+          locations: {
+            id: "gid://shopify/Metafield/44429081411911",
+            value: "{}",
+          },
+          bookingPeriodValue: {
+            id: "gid://shopify/Metafield/44429081313607",
+            value: "1",
+          },
+          bookingPeriodUnit: {
+            id: "gid://shopify/Metafield/44429081280839",
+            value: "months",
+          },
+          noticePeriodValue: {
+            id: "gid://shopify/Metafield/44429081477447",
+            value: "1",
+          },
+          noticePeriodUnit: {
+            id: "gid://shopify/Metafield/44429081444679",
+            value: "hours",
+          },
+          duration: {
+            id: "gid://shopify/Metafield/44429081379143",
+            value: "60",
+          },
+          breaktime: {
+            id: "gid://shopify/Metafield/44429081346375",
+            value: "10",
+          },
+        },
+      },
+    };
   });
 
   it("should be able to add slots schedule", async () => {
     const user = await createUser({ customerId });
+
+    const newSchedule = await CustomerScheduleServiceCreate({
+      name: "asd",
+      customerId,
+    });
+
+    const tags = [
+      `user`,
+      `user-${user.username}`,
+      `userid-${user.customerId}`,
+      `treatments`,
+      `productid-${GidFormat.parse(
+        mockProduct.productDuplicate?.newProduct?.id
+      )}`,
+      `product-${mockProduct.productDuplicate?.newProduct?.handle}`,
+      `scheduleid-${newSchedule._id}`,
+      `locationid-${productBody.locations[0].location}`,
+    ];
+
+    const mockProductUpdate: ProductUpdateMutation = {
+      productUpdate: {
+        product: {
+          id: `gid://shopify/Product/${GidFormat.parse(
+            mockProduct.productDuplicate?.newProduct?.id
+          )}}`,
+          variants: {
+            nodes: mockProduct.productDuplicate?.newProduct?.variants.nodes!,
+          },
+          tags,
+          parentId: {
+            id: mockProduct.productDuplicate?.newProduct?.parentId?.id!,
+            value: `gid://shopify/Product/${productBody.parentId}`,
+          },
+          scheduleId: {
+            id: mockProduct.productDuplicate?.newProduct?.scheduleId?.id!,
+            value: "schedule",
+          },
+          locations: {
+            id: mockProduct.productDuplicate?.newProduct?.locations?.id!,
+            value: '{"locations":[]}',
+          },
+          bookingPeriodValue: {
+            id: mockProduct.productDuplicate?.newProduct?.bookingPeriodValue
+              ?.id!,
+            value: "1",
+          },
+          bookingPeriodUnit: {
+            id: mockProduct.productDuplicate?.newProduct?.bookingPeriodUnit
+              ?.id!,
+            value: "months",
+          },
+          noticePeriodValue: {
+            id: mockProduct.productDuplicate?.newProduct?.noticePeriodValue
+              ?.id!,
+            value: "1",
+          },
+          noticePeriodUnit: {
+            id: mockProduct.productDuplicate?.newProduct?.noticePeriodUnit?.id!,
+            value: "hours",
+          },
+          duration: {
+            id: mockProduct.productDuplicate?.newProduct?.duration?.id!,
+            value: "60",
+          },
+          breaktime: {
+            id: mockProduct.productDuplicate?.newProduct?.breaktime?.id!,
+            value: "10",
+          },
+        },
+      },
+    };
+
+    const mockProductPriceUpdate: ProductPricepdateMutation = {
+      productVariantsBulkUpdate: {
+        product: {
+          id: `gid://shopify/Product/${GidFormat.parse(
+            mockProduct.productDuplicate?.newProduct?.id
+          )}}`,
+          variants: {
+            nodes: [
+              {
+                id: mockProduct.productDuplicate?.newProduct?.variants.nodes[0]
+                  .id!,
+                compareAtPrice: productBody.compareAtPrice.amount,
+                price: productBody.price.amount,
+              },
+            ],
+          },
+        },
+      },
+    };
 
     mockRequest
       .mockResolvedValueOnce({
         data: mockProduct,
       })
       .mockResolvedValueOnce({
-        data: {
-          productUpdate: {
-            product: {
-              id: mockProduct.productDuplicate?.newProduct?.id,
-              tags: [
-                "user",
-                "treatments",
-                `user-${user.username}`,
-                `customer-${user.customerId}`,
-                `product-${GidFormat.parse(
-                  mockProduct.productDuplicate?.newProduct?.id
-                )}`,
-                `product-${mockProduct.productDuplicate?.newProduct?.handle}`,
-              ],
-            },
-          },
-        },
+        data: mockProductUpdate,
+      })
+      .mockResolvedValueOnce({
+        data: mockProductPriceUpdate,
       });
-
-    const newSchedule = await CustomerScheduleServiceCreate({
-      name: "asd",
-      customerId,
-    });
 
     const body = getProductObject({ productId });
     request = await createHttpRequest<CustomerProductControllerAddRequest>({
