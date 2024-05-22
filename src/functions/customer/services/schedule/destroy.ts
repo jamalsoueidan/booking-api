a149f031007b5c3aaac43746387a5f04bdaa81d7
Jamal Soueidan <jamal@soueidan.com>
2024-05-22 13:10:51 +0200

    destroy schedule metaobject when deleting the document

--- src/functions/customer/services/schedule/destroy.ts
+++ src/functions/customer/services/schedule/destroy.ts
@@ -1,4 +1,5 @@
 import { Schedule, ScheduleModel } from "~/functions/schedule";
+import { shopifyAdmin } from "~/library/shopify";
 
 export type CustomerScheduleServiceDestroyProps = {
   scheduleId: Schedule["_id"];
@@ -8,8 +9,29 @@ export type CustomerScheduleServiceDestroyProps = {
 export const CustomerScheduleServiceDestroy = async (
   props: CustomerScheduleServiceDestroyProps
 ) => {
+  const schedule = await ScheduleModel.findOne({
+    _id: props.scheduleId,
+    customerId: props.customerId,
+  });
+
+  if (schedule && schedule.metafieldId) {
+    await shopifyAdmin.request(DESTROY_SCHEDULE_METAFIELD, {
+      variables: {
+        metafieldId: schedule.metafieldId,
+      },
+    });
+  }
+
   return ScheduleModel.deleteOne({
     _id: props.scheduleId,
     customerId: props.customerId,
   });
 };
+
+export const DESTROY_SCHEDULE_METAFIELD = `#graphql
+  mutation destroyScheduleMetafield($metafieldId: ID!){
+    metafieldDelete(input: {id: $metafieldId}) {
+      deletedId
+    }
+  }
+` as const;
