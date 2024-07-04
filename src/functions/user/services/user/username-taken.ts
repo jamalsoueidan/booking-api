327d5c7509ffc5c7eb46008062d514bd1ca02f72
Jamal Soueidan <jamal@soueidan.com>
2024-07-04 14:17:27 +0200

    Add minimum length check for username in UserServiceUsernameTaken function

--- src/functions/user/services/user/username-taken.ts
+++ src/functions/user/services/user/username-taken.ts
@@ -6,6 +6,7 @@ export type UserServiceUsernameTakenProps = Required<Pick<User, "username">>;
 export const UserServiceUsernameTaken = async ({
   username,
 }: UserServiceUsernameTakenProps) => {
+  if (username.length < 4) return { usernameTaken: true };
   const usernameTaken = await UserModel.findOne({ username }).lean();
   return { usernameTaken: !!usernameTaken };
 };
